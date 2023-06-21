e0de1e0e25611b3c1f8d03ec07ad85a4b3559950
Greg <mistzero0@gmail.com>
2023-06-21 21:38:42 +0800

    modal to delete item

--- resources/views/desa.blade.php
+++ resources/views/desa.blade.php
@@ -2,6 +2,7 @@
 
 @section('container')
     <section class="w-full overflow-auto rounded-2xl border bg-white">
+
         <table class="stable-fixed h-full w-full border-collapse">
             <thead class="font-medium">
                 <tr>
@@ -15,23 +16,65 @@
 
             <tbody>
                 @foreach ($data as $d)
-                <tr>
-                    <td>{{$d["nik"]}}</td>
-                    <td>{{$d["name"]}}</td>
-                    <td>{{$d["address"]}}</td>
-                    <td>{{$d["jenis_kelamin"]}}</td>
-                    
-                    <td class="">
-                        <button title='Dokumen'><i class="bi bi-file-earmark-fill mr-3 text-emerald-400 hover:text-emerald-500"></i></button>
-                        <a href='edit/9'><i class="bi bi-pencil-fill mr-3 text-amber-400 hover:text-amber-500" title='Edit'></i></a>
-                        <button><i class="bi bi-trash-fill text-rose-400 hover:text-rose-500" title='Hapus'></i></button>
-                    </td>
-                </tr>
+                    <tr>
+                        <td>{{ $d['nik'] }}</td>
+                        <td>{{ $d['name'] }}</td>
+                        <td>{{ $d['address'] }}</td>
+                        <td>{{ $d['jenis_kelamin'] }}</td>
+
+                        <td class="">
+                            <button title='Dokumen'><i
+                                    class="bi bi-file-earmark-fill mr-3 text-emerald-400 hover:text-emerald-500"></i></button>
+                            <a href='edit/9'><i class="bi bi-pencil-fill mr-3 text-amber-400 hover:text-amber-500"
+                                    title='Edit'></i></a>
+                            <button class="openModalButton" onclick="buttonClick('lmao')"><i
+                                    class="bi bi-trash-fill text-rose-400 hover:text-rose-500" title='Hapus'></i></button>
+                        </td>
+                    </tr>
                 @endforeach
-                
+
+
             </tbody>
             <tfoot></tfoot>
 
         </table>
+        <div class="modal fixed top-0 left-0 z-50 h-screen w-screen bg-black bg-opacity-30" id="myModal"
+            style="display: none">
+            <div class="flex h-full w-full items-center justify-center" onclick="hide()">
+                <div class="modal-content relative max-w-xl rounded-lg bg-white p-10" onclick="stopPropagation(event)">
+                    <button class="close absolute -top-4 -right-4 h-8 w-8 rounded-full bg-white shadow-lg">&times;</button>
+                    <h2 class="text-xl font-medium">Hapus</h2>
+                    <p>Apakah anda yakin untuk menghapus?</p>
+                    <form action="/penduduk/delete/x" method="POST">
+
+                        <input name='id' value="" class="hidden">
+                        <button class="mt-6 rounded-md bg-red-400 px-6 py-1 font-semibold text-white"
+                            type="submit">Hapus</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+        <script>
+            function hide() {
+                document.getElementById("myModal").style.display = "none";
+
+            }
+
+            function stopPropagation(event) {
+                event.stopPropagation()
+            }
+            document.querySelector(".close").addEventListener("click", function() {
+                document.getElementById("myModal").style.display = "none";
+
+
+            });
+
+            function buttonClick(message) {
+                document.getElementById("myModal").style.display = "block";
+                console.log(message)
+
+            }
+        </script>
     </section>
 @endsection
